9b383750e47a4915079b93a267174de266ab4c82
rohitBlueYonder <rohit.rai@jda.com>
2022-09-26 14:14:24 +0530

    jdbc

---
+++ src/main/java/com/spring/boot/jdbc/SpringBootJDBCProject/SpringBootJdbcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.jdbc.SpringBootJDBCProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/boot/jdbc/SpringBootJDBCProject/SpringBootJdbcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.jdbc.SpringBootJDBCProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
